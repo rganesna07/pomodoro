6df79a44ad36a82e35a59c22aaa6060e4c2aa7c2
Rithika Ganesna <ganesnar@oregonstate.edu>
2024-07-22 12:39:55 -0700

    added functionality to buttons and timer functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,84 @@
 // import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import { Button } from 'react-bootstrap';
+import { Howl } from 'howler';
 
+import clockMp3 from '../src/media/mixkit-sleepy-cat-135.mp3'
+import bellMp3 from '../src/media/bell.mp3'
+
+let clockLoop = new Howl({
+  src: [clockMp3],
+  loop: true
+})
+
+let bellSound = new Howl({
+  src: [bellMp3],
+})
 function App() {
   const [isRunning, setRunning]= useState(false);
   const [minutes, setMinutes] = useState(25);
   const [seconds, setSeconds] = useState(0);
+  
+
+
+  useEffect(() => {
+    if (isRunning){
+      const interval = setInterval(()=> {
+        
+        if(seconds >0){
+          setSeconds((seconds) => seconds -1)
+        }
+
+        if (seconds === 0){
+          setMinutes((minutes) => minutes -1)
+          setSeconds(59)
+        }
+
+        if (minutes === 0 && seconds === 0){
+          setMinutes(0)
+          setSeconds(0)
+          setRunning(false)
+          clockLoop.stop()
+          bellSound.play()
+        }
+
+      }, 1000)
+      return () => clearInterval(interval)
+      
+    }
+  
+  },[isRunning, minutes, seconds])
+
+  const decrease = () => {
+    if (minutes > 0){
+      setMinutes((minutes) => minutes-1)
+    }
+  }
+
+  const increase = () => {
+    if (minutes < 60){
+    setMinutes((minutes) => minutes+1)
+    }
+  }
+
+  const startTimer = () => {
+    clockLoop.play()
+    setRunning(true)
+  }
+
+  const PauseTimer = () => { 
+    setRunning(false)
+    clockLoop.stop()
+  }
+
+  const resetTimer = () => {
+    clockLoop.stop()
+    setRunning(false)
+    setMinutes(25)
+    setSeconds(0)
+
+  }
 
   return (
     <div className= 'container'>
@@ -17,12 +89,17 @@ function App() {
         
       </div>
       <div className= 'timerArea'>
-        <h2 className='timerDisplay'> 25:00</h2>
+        <h2 className='timerDisplay'> {minutes}:{seconds < 10 ? "0" +seconds: seconds}</h2>
+        <div className= 'cntrlTimer'>
+          <button className='cntrlBtn' onClick={increase}> + </button>
+          <button className= 'cntrlBtn' onClick={decrease}> - </button>
+          
+        </div>
       </div>
      <div className='Buttons'>
-      <button className='Start'>Start</button>
-      <button className='Reset'>Reset</button>
-      <button className='Pause'>Pause</button>
+      <button className='Start' onClick={startTimer}>Start</button>
+      <button className='Reset' onClick={resetTimer}>Reset</button>
+      <button className='Pause' onClick={PauseTimer}>Pause</button>
 
      </div>
     </div>
